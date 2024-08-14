86eb648fd316b9b167a0f4d3f87ff2359ecf1d68
Iliyas M <13122679+smiliyas@users.noreply.github.com>
2024-08-14 15:58:23 +0530

    fix async alpine not loading reliably in spa mode

--- resources/views/phone-input.blade.php
+++ resources/views/phone-input.blade.php
@@ -71,7 +71,7 @@
                 class="w-full"
                 x-ignore
                 @if (FilamentView::hasSpaMode())
-                    ax-load="visible"
+                    {{-- format-ignore-start --}}ax-load="visible || event (ax-modal-opened)" {{-- format-ignore-end --}}
                 @else
                     ax-load
                 @endif
